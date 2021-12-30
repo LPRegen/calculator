3b9e2fceaa8abdc87ed6cfd8234a68c02e92887b
Manuel <manuel@pop-os.localdomain>
2021-12-30 09:35:57 -0300

    Add equalsbtn function
    When user press equals button the operation with selected operator is performed

--- main.js
+++ main.js
@@ -23,9 +23,42 @@ let updateDislpay = function (dataAction, btnContent) {
   }
 };
 
+// Calculate
+// ! Refactor
+let equalsBtn = function (dataAction, selectedOperator) {
+  if (dataAction === 'calculate') {
+    if (selectedOperator === 'divide') {
+      total =
+        parseFloat(display.dataset.lastNumber) /
+        parseFloat(display.textContent);
+      display.textContent = total;
+      checkAction(dataAction);
+    } else if (selectedOperator === 'add') {
+      total =
+        parseFloat(display.dataset.lastNumber) +
+        parseFloat(display.textContent);
+      display.textContent = total;
+      checkAction(dataAction);
+    } else if (selectedOperator === 'multiply') {
+      total =
+        parseFloat(display.dataset.lastNumber) *
+        parseFloat(display.textContent);
+      display.textContent = total;
+      checkAction(dataAction);
+    } else if (selectedOperator === 'subtract') {
+      total =
+        parseFloat(display.dataset.lastNumber) -
+        parseFloat(display.textContent);
+      display.textContent = total;
+      checkAction(dataAction);
+    }
+  }
+};
+
 // Check if user press an operator
 let checkAction = function (dataAction) {
   if (operators.includes(`${dataAction}`)) {
+    display.dataset.operator = dataAction;
     display.dataset.lastNumber = display.textContent;
     display.textContent = '0';
   }
@@ -37,9 +70,11 @@ calculatorBtns.forEach((el) => {
     if (target.matches('button')) {
       let dataAction = target.dataset.action;
       let btnContent = target.textContent;
+      let selectedOperator = display.dataset.operator;
 
       updateDislpay(dataAction, btnContent);
       checkAction(dataAction);
+      equalsBtn(dataAction, selectedOperator);
     }
   });
 });
